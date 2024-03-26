7272f250c61df3075d762598b0fca735029da3da
pschlo <70105856+pschlo@users.noreply.github.com>
2024-03-26 19:31:55 +0100

    added export error handling

--- pywarden/cli/services/import_export_service.py
+++ pywarden/cli/services/import_export_service.py
@@ -5,4 +5,7 @@ from ..service import Service
 class ImportExportService(Service):
   def get_export(self) -> str:
     r = self.conn.run_command(['export', '--format', 'json', '--raw'])
-    return r.stdout.decode()
+    data = r.stdout.decode().strip()
+    if not data:
+      raise RuntimeError(f"Export failed")
+    return data
